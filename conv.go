305effeeb673c4f3cbb16c49c4b3ec0d373602e5
AlasdairF <AlasdairF@users.noreply.github.com>
2015-12-22 22:59:31 +0700

    Changed to use sync.pool

--- conv.go
+++ conv.go
@@ -1,174 +1,16 @@
 package conv
 
+import "sync"
+
 const (
  digits01 = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
  digits10 = "0000000000111111111122222222223333333333444444444455555555556666666666777777777788888888889999999999"
 )
 
-type Buf struct {
- buf [20]byte
-}
-
-func NewConverter() *Buf {
-	obj := new(Buf)
-	return obj
-}
-
-func (obj *Buf) String(u int) string {
-	return string(obj.format(u, 0))
-}
-
-func (obj *Buf) StringPad(u int, p int) string {
-	return string(obj.format(u, p))
-}
-
-func (obj *Buf) Bytes(u int) []byte {
-	return obj.format(u, 0)
-}
-
-func (obj *Buf) BytesPad(u int, p int) []byte {
-	return obj.format(u, p)
-}
-
-func (obj *Buf) FloatString(f float64, prec int) string {
-	return string(obj.FloatBytes(f, prec))
-}
-
-func (obj *Buf) FloatBytes(f float64, prec int) []byte {
-	if prec == 0 {
-	  return obj.format(int(f), 0)
-	}
-	u := int(f)
-	save := u
-	var neg bool
-	if u < 0 {
-		neg = true
-		u = -u
-	}
-
-	var q int
-	var j uintptr
-	a := obj.buf[0:20]
-	i := 19 - prec
-
-	for u >= 100 {
-		i -= 2
-		q = u / 100
-		j = uintptr(u - q*100)
-		a[i+1] = digits01[j]
-		a[i] = digits10[j]
-		u = q
-	}
-	if u >= 10 {
-		i--
-		q = u / 10
-		a[i] = digits01[uintptr(u-q*10)]
-		u = q
-	}
-	i--
-	a[i] = digits01[uintptr(u)]
-	
-
-	if neg {
-		i--
-		a[i] = '-'
-	}
-	
-	a[19 - prec] = '.'
-	switch prec {
-		case 1: u = int(f * 10) - (save * 10)
-		case 2: u = int(f * 100) - (save * 100)
-		case 3: u = int(f * 1000) - (save * 1000)
-		case 4: u = int(f * 10000) - (save * 10000)
-		case 5: u = int(f * 100000) - (save * 100000)
-		case 6: u = int(f * 1000000) - (save * 1000000)
-		case 7: u = int(f * 10000000) - (save * 10000000)
-		case 8: u = int(f * 100000000) - (save * 100000000)
-		case 9: u = int(f * 1000000000) - (save * 1000000000)
-	}
-	if neg {
-		u = -u
-	}
-	save = i
-	
-	i = 20
-	for u >= 100 {
-		i -= 2
-		q = u / 100
-		j = uintptr(u - q*100)
-		a[i+1] = digits01[j]
-		a[i] = digits10[j]
-		u = q
-	}
-	if u >= 10 {
-		i--
-		q = u / 10
-		a[i] = digits01[uintptr(u-q*10)]
-		u = q
-	}
-	i--
-	a[i] = digits01[uintptr(u)]
-	
-	return a[save:]
-}
-
-func (obj *Buf) format(u int, padding int) []byte {
-
-	var neg bool
-	if u < 0 {
-		neg = true
-		u = -u
-	} else {
-		if u < 10 && padding == 0 {
-			return []byte{byte(u) + 48}
-		}
-	}
-
-	var q int
-	var j uintptr
-	a := obj.buf[0:20]
-	i := 20
-
-	for u >= 100 {
-		i -= 2
-		q = u / 100
-		j = uintptr(u - q*100)
-		a[i+1] = digits01[j]
-		a[i] = digits10[j]
-		u = q
-	}
-	if u >= 10 {
-		i--
-		q = u / 10
-		a[i] = digits01[uintptr(u-q*10)]
-		u = q
-	}
-	i--
-	a[i] = digits01[uintptr(u)]
-	
-	if padding == 0 {
-		if neg {
-			i--
-			a[i] = '-'
-		}
-		return a[i:]
-	}
-	
-	if neg {
-		padding = 21 - padding
-	} else {
-		padding = 20 - padding
-	}
-	for i > padding {
-		i--
-		a[i] = '0'
-	}
-	if neg {
-		i--
-		a[i] = '-'
-	}
-	
-	return a[i:]
+var pool = sync.Pool{
+    New: func() interface{} {
+        return make([]byte, 20)
+    },
 }
 
 func String(u int) string {
@@ -205,7 +47,8 @@ func FloatBytes(f float64, prec int) []byte {
 
 	var q int
 	var j uintptr
-	var a [20]byte
+	a := pool.Get().([]byte)
+	defer pool.Put(a)
 	i := 19 - prec
 
 	for u >= 100 {
@@ -225,7 +68,6 @@ func FloatBytes(f float64, prec int) []byte {
 	i--
 	a[i] = digits01[uintptr(u)]
 	
-
 	if neg {
 		i--
 		a[i] = '-'
@@ -266,11 +108,12 @@ func FloatBytes(f float64, prec int) []byte {
 	i--
 	a[i] = digits01[uintptr(u)]
 	
-	return a[save:]
+	cpy := make([]byte, 20 - save)
+	copy(cpy, a[save:])
+	return cpy
 }
 
 func format(u int, padding int) []byte {
-
 	var neg bool
 	if u < 0 {
 		neg = true
@@ -283,7 +126,8 @@ func format(u int, padding int) []byte {
 
 	var q int
 	var j uintptr
-	var a [20]byte
+	a := pool.Get().([]byte)
+	defer pool.Put(a)
 	i := 20
 
 	for u >= 100 {
@@ -308,7 +152,9 @@ func format(u int, padding int) []byte {
 			i--
 			a[i] = '-'
 		}
-		return a[i:]
+		cpy := make([]byte, 20 - i)
+		copy(cpy, a[i:])
+		return cpy
 	}
 	
 	if neg {
@@ -325,7 +171,69 @@ func format(u int, padding int) []byte {
 		a[i] = '-'
 	}
 	
-	return a[i:]
+	cpy := make([]byte, 20 - i)
+	copy(cpy, a[i:])
+	return cpy
+}
+
+func Write(w io.Writer, u int, padding int) (int, error) {
+	a := pool.Get().([]byte)
+	defer pool.Put(a)
+	var neg bool
+	if u < 0 {
+		neg = true
+		u = -u
+	} else {
+		if u < 10 && padding == 0 {
+			a[0] = byte(u) + 48
+			return w.Write(a[0:1])
+		}
+	}
+
+	var q int
+	var j uintptr
+	i := 20
+
+	for u >= 100 {
+		i -= 2
+		q = u / 100
+		j = uintptr(u - q*100)
+		a[i+1] = digits01[j]
+		a[i] = digits10[j]
+		u = q
+	}
+	if u >= 10 {
+		i--
+		q = u / 10
+		a[i] = digits01[uintptr(u-q*10)]
+		u = q
+	}
+	i--
+	a[i] = digits01[uintptr(u)]
+	
+	if padding == 0 {
+		if neg {
+			i--
+			a[i] = '-'
+		}
+		return w.Write(a[i:])
+	}
+	
+	if neg {
+		padding = 21 - padding
+	} else {
+		padding = 20 - padding
+	}
+	for i > padding {
+		i--
+		a[i] = '0'
+	}
+	if neg {
+		i--
+		a[i] = '-'
+	}
+	
+	return w.Write(a[i:])
 }
 
 func Int(a []byte) (result int) {
