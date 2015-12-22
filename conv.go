b079b036f62812230d691ed3ae108cf53cd74826
AlasdairF <AlasdairF@users.noreply.github.com>
2015-12-23 00:23:16 +0700

    Added WriteFloat

--- conv.go
+++ conv.go
@@ -239,6 +239,84 @@ func Write(w io.Writer, u int, padding int) (int, error) {
 	return w.Write(a[i:])
 }
 
+func WriteFloat(w io.Writer, f float64, prec int) (int, error) {
+	if prec == 0 {
+	  return Write(w, int(f), 0)
+	}
+	u := int(f)
+	save := u
+	var neg bool
+	if u < 0 {
+		neg = true
+		u = -u
+	}
+
+	var q int
+	var j uintptr
+	a := pool.Get().([]byte)
+	defer pool.Put(a)
+	i := 19 - prec
+
+	for u >= 100 {
+		i -= 2
+		q = u / 100
+		j = uintptr(u - q*100)
+		a[i+1] = digits01[j]
+		a[i] = digits10[j]
+		u = q
+	}
+	if u >= 10 {
+		i--
+		q = u / 10
+		a[i] = digits01[uintptr(u-q*10)]
+		u = q
+	}
+	i--
+	a[i] = digits01[uintptr(u)]
+	
+	if neg {
+		i--
+		a[i] = '-'
+	}
+	
+	a[19 - prec] = '.'
+	switch prec {
+		case 1: u = int(f * 10) - (save * 10)
+		case 2: u = int(f * 100) - (save * 100)
+		case 3: u = int(f * 1000) - (save * 1000)
+		case 4: u = int(f * 10000) - (save * 10000)
+		case 5: u = int(f * 100000) - (save * 100000)
+		case 6: u = int(f * 1000000) - (save * 1000000)
+		case 7: u = int(f * 10000000) - (save * 10000000)
+		case 8: u = int(f * 100000000) - (save * 100000000)
+		case 9: u = int(f * 1000000000) - (save * 1000000000)
+	}
+	if neg {
+		u = -u
+	}
+	save = i
+	
+	i = 20
+	for u >= 100 {
+		i -= 2
+		q = u / 100
+		j = uintptr(u - q*100)
+		a[i+1] = digits01[j]
+		a[i] = digits10[j]
+		u = q
+	}
+	if u >= 10 {
+		i--
+		q = u / 10
+		a[i] = digits01[uintptr(u-q*10)]
+		u = q
+	}
+	i--
+	a[i] = digits01[uintptr(u)]
+	
+	return w.Write(a[save:])
+}
+
 func Int(a []byte) (result int) {
 	if len(a) == 0 {
 		return 0
